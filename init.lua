f6ee6236fa4807579c65a63655ca4e990951ec4c
Pandademic <atharv.gupte510@gmail.com>
2022-01-10 09:21:26 -0500

    fix runtime path - now just gotta figure settings loading

--- init.lua
+++ init.lua
@@ -4,16 +4,17 @@
 -- __          __             ______   ______   _        ______    __      __  _____   __  __ 
 -- \ \        / /     /\     |  ____| |  ____| | |      |  ____|   \ \    / / |_   _| |  \/  |
 --  \ \  /\  / /     /  \    | |__    | |__    | |      | |__       \ \  / /    | |   | \  / |
---   \ \/  \/ /     / /\ \   |  __|   |  __|   | |      |  __|       \ \/ /     | |   | |\/| |
---   \  /\  /     / ____ \  | |      | |      | |____  | |____       \  /     _| |_  | |  | |
---     \/  \/     /_/    \_\ |_|      |_|      |______| |______|       \/     |_____| |_|  |_|
+--   \ \/  \/ /     / /\ \   |  __|   |  __|   | |      |  __|       \ \/ /     | |   | |\/| |  
+--   \  /\  /      / ____ \  | |      | |      | |____  | |____       \  /     _| |_  | |  | |  
+--     \/  \/     /_/    \_\ |_|      |_|      |______| |______|       \/     |_____| |_|  |_|     
+--                                							       
 --                                                                                            
 --                                                                                            
 -- (c) ATHARV GUPTE 2022 - MIT LICENCED
 --
 -- 
 
-vim.opt.runtimepath=$HOME/.vim,$VIM/vimfiles,$VIMRUNTIME,$VIM/vimfiles/after,$HOME/.vim/after
+vim.opt.runtimepath="$HOME/.vim,$VIM/vimfiles,$VIMRUNTIME,$VIM/vimfiles/after,$HOME/.vim/after"
 local settings = require 'wafflesettings.setting'
 if settings.StartMode == "default" then
  require('modes.default.plugins')

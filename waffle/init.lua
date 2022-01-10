5c73bc96a67a217e891e3bfe209fafeb9da56a0f
Pandademic <74566464+Pandademic@users.noreply.github.com>
2022-01-10 15:29:42 -0500

    Update init.lua

--- waffle/init.lua
+++ waffle/init.lua
@@ -3,13 +3,13 @@ local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
 local g = vim.g      -- a table to access global variables
 local opt = vim.opt  -- to set options
 
-local syrup = require('syrup')
+local vo = require('vim-options.lua')
 
 -- load the user options {
-opt.cursorline = syrup.settings.cursorline
-opt.tabstop =  syrup.settings.tabwidth
-opt.autoindent = syrup.settings.autoindent
-vim.wo.number = syrup.settings.numbers
+opt.cursorline = vo.cursorline
+opt.tabstop =  vo.tabwidth
+opt.autoindent = vo.autoindent
+vim.wo.number = vo.numbers
  
 
 --- }
@@ -34,9 +34,9 @@ if modes.default == true then
    require "paq" {
        "savq/paq-nvim";                  -- Let Paq manage itself
 
-       "VonHeikemen/fine-cmdline.nvim";
+       "VonHeikemen/fine-cmdline.nvim";  -- nice command option
             
-        "MunifTanjim/nui.nvim";
+        "MunifTanjim/nui.nvim"; -- dependency 
 
    }
    vim.api.nvim_set_keymap('n', ':', '<cmd>FineCmdline<CR>', {noremap = true})

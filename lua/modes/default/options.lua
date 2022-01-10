d1c912a7a68cb4cb71de36358a19627dcca53340
Pandademic <74566464+Pandademic@users.noreply.github.com>
2022-01-10 09:40:39 -0500

    Delete lua directory

--- lua/modes/default/options.lua
+++
@@ -1,3 +0,0 @@
--- THE DEAFULT USED VIM OPTIONS
-vim.opt.autoindent = true --auto indent
-vim.opt.cursorline = false -- NO cursorline
--- lua/wafflesettings/setting.lua
+++
@@ -1,7 +0,0 @@
--- global settings
-vim.o.background = 'dark' -- background
-vim.g.mapleader = ‘,’ -- leader key
-local waffle_settings = {
-  StartupMode = "default",
-}
-return waffle_settings

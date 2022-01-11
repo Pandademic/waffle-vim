dbe8ca08c44a0fcdbab4bd26084d51e55e73fabd
Pandademic <74566464+Pandademic@users.noreply.github.com>
2022-01-11 09:44:46 -0500

    Update init.lua

--- waffle/init.lua
+++ waffle/init.lua
@@ -79,21 +79,7 @@ if modes.ide == true then
        
     
   }
-  require("sidebar-nvim").setup({
-    disable_default_keybindings = 0,
-    bindings = nil,
-    open = false,
-    side = "left",
-    initial_width = 35,
-    update_interval = 1000,
-    sections = { "datetime", "git-status", "lsp-diagnostics" },
-    section_separator = "-----",
-    containers = {
-        attach_shell = "/bin/sh", show_all = true, interval = 5000,
-    },
-    datetime = { format = "%a %b %d, %H:%M", clocks = { { name = "local" } } },
-    todos = { ignored_paths = { "~" } }
-})
+
   cmd([[
       map <c-t> command terminal
    ]])

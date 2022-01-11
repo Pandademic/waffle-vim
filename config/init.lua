09665212342ba20838c39d5a15493919a55fc34c
Pandademic <74566464+Pandademic@users.noreply.github.com>
2022-01-11 11:18:07 -0500

    Update init.lua

--- config/init.lua
+++ config/init.lua
@@ -1,1 +1,26 @@
 -- your waffle's toppings A.K.A your config
+-- TOPPINGS (your config) {
+-- options for vim 
+vo = {
+  tabwidth=4,
+  autoindent=true,
+  colorscheme="default",
+  cursorline=false,
+  numbers=false,
+  smartindent=true,
+  swapfile=false,
+  backup=false,
+  smartcase=true,
+  bells=false, -- error bells
+  syntax='on', -- on or off
+  leaderKey=';',
+  
+}
+-- modes {
+modes = {
+   default = true,
+   ide = false,
+   tranquil = nil,
+}
+
+-- }

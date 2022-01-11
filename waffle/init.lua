9f60c4671dcd3c3c53244fceb60f20c6bc4e8b0a
Pandademic <74566464+Pandademic@users.noreply.github.com>
2022-01-11 09:43:11 -0500

    Update init.lua

--- waffle/init.lua
+++ waffle/init.lua
@@ -74,9 +74,26 @@ if modes.ide == true then
         "Konfekt/FastFold"; --speed up vim
     
         "antoinemadec/FixCursorHold.nvim"; -- fix
+    
+        "sidebar-nvim/sidebar.nvim"; -- ide sidebar
        
     
   }
+  require("sidebar-nvim").setup({
+    disable_default_keybindings = 0,
+    bindings = nil,
+    open = false,
+    side = "left",
+    initial_width = 35,
+    update_interval = 1000,
+    sections = { "datetime", "git-status", "lsp-diagnostics" },
+    section_separator = "-----",
+    containers = {
+        attach_shell = "/bin/sh", show_all = true, interval = 5000,
+    },
+    datetime = { format = "%a %b %d, %H:%M", clocks = { { name = "local" } } },
+    todos = { ignored_paths = { "~" } }
+})
   cmd([[
       map <c-t> command terminal
    ]])

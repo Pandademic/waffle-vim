a7b0657b7869a84dc91d03947ef1a887c683640e
Atharv Gupte <74566464+Pandademic@users.noreply.github.com>
2022-01-23 09:29:51 -0500

    Update ide.lua

--- modes/ide.lua
+++ modes/ide.lua
@@ -44,9 +44,7 @@ function configurePlugins()
           lualine_a = {'mode'},
           lualine_b = {'branch', 'diff', 'diagnostics'},
           lualine_c = {'filename'},
-	  lualine_d = {
-		{ gps.get_location, cond = gps.is_available },
-		}
+	  lualine_d = {{ gps.get_location, cond = gps.is_available },},
           lualine_x = {'encoding', 'fileformat', 'filetype'},
           lualine_y = {'progress'},
           lualine_z = {'location'}

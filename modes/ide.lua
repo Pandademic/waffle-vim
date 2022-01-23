2acd38aafa8f2adb87cbc3e3391b56598a830d39
Atharv Gupte <74566464+Pandademic@users.noreply.github.com>
2022-01-23 09:20:44 -0500

    Update ide.lua

--- modes/ide.lua
+++ modes/ide.lua
@@ -1,26 +1,13 @@
 function installPlugins()
-    return require('packer').init(
-      {
-         display = {
-        non_interactive = true, -- If true, disable display windows for all operations
-        open_fn  = nil, -- An optional function to open a window for packer's display
-        open_cmd = '65vnew \\[packer\\]', -- An optional command to open a window for packer's display
-        working_sym = '⟳', -- The symbol for a plugin being installed/updated
-        error_sym = '✗', -- The symbol for a plugin with an error in installation/updating
-        done_sym = '✓', -- The symbol for a plugin which has completed installation/updating
-        removed_sym = '-', -- The symbol for an unused plugin which was removed
-        moved_sym = '→', -- The symbol for a plugin which was moved (e.g. from opt to start)
-        header_sym = '━', -- The symbol for the header line in packer's display
-        show_all_info = true, -- Should packer show all update details automatically?
-        prompt_border = 'double', -- Border style of prompt popups.
-        keybindings = { -- Keybindings for the display window
-        quit = 'q',
-        toggle_info = '<CR>',
-        diff = 'd',
-        prompt_revert = 'r',
+    -- Have packer use a popup window
+    packer.init({
+        display = {
+        open_fn = function()
+            return require('packer.util').float({ border = 'single' })
+        end
         }
-                }
-    })
+     }
+    )
     return require('packer').startup(function()
         -- Packer can manage itself
         use 'wbthomason/packer.nvim'

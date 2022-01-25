11012974f33e644e30eac8ec5fee71ee1529e21a
Atharv Gupte <74566464+Pandademic@users.noreply.github.com>
2022-01-24 20:34:46 -0500

    Update tranquil.lua

--- modes/tranquil.lua
+++ modes/tranquil.lua
@@ -22,7 +22,16 @@ function installPlugins()
         use 'junegunn/goyo.vim'
       
         use 'junegunn/limelight.vim'
-            
+                    
+        use 'lewis6991/impatient.nvim'
+        
+        packer.startup{{...},
+        config = {
+                -- Move to lua dir so impatient.nvim can cache it
+                compile_path = vim.fn.stdpath('config')..'/lua/packer_compiled.lua'
+                }     
+        }
+
       end)
         
 end

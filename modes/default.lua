12c936951e20335ac09b39afd86f10186fb5fb32
Atharv Gupte <74566464+Pandademic@users.noreply.github.com>
2022-01-24 20:33:29 -0500

    Update default.lua

--- modes/default.lua
+++ modes/default.lua
@@ -18,7 +18,13 @@ function installPlugins()
         use 'Konfekt/FastFold' --  speed up vim
             
         use 'tpope/vim-fugitive' -- git wrapping
-            
+        
+         use 'lewis6991/impatient.nvim'
+                    
+          config = {
+        -- Move to lua dir so impatient.nvim can cache it
+        compile_path = vim.fn.stdpath('config')..'/lua/packer_compiled.lua'
+  }
       end)
         
 end

83b431a9ed83e37f80a91a27be197927f9a8486e
Pandademic <atharv.gupte510@gmail.com>
2022-01-25 18:34:11 -0500

    fix everything and add new plugins to tranqui;

--- modes/ide/init.lua
+++ modes/ide/init.lua
@@ -10,11 +10,11 @@ function installPlugins()
         -- Packer can manage itself
         use 'wbthomason/packer.nvim'
      
-	use 'lewis6991/impatient.nvim'
+	    use 'lewis6991/impatient.nvim' -- speed up your vim startup , really badly needed for ide mode
 			
-        use 'antoinemadec/FixCursorHold.nvim' -- generic dependencie
+        use 'antoinemadec/FixCursorHold.nvim' -- TODO: check if this is really needed
 
-        use 'Konfekt/FastFold' --  speed up vim
+        use 'Konfekt/FastFold' --  speed up your time inside of vim
 
         use {
             'nvim-lualine/lualine.nvim',
@@ -37,12 +37,50 @@ function installPlugins()
       end)
         
 end
+function configure()
+    require('lualine').setup {
+        options = {
+          icons_enabled = true,
+          theme = 'wombat',
+          component_separators = { left = '', right = ''},
+          section_separators = { left = '', right = ''},
+          disabled_filetypes = {},
+          always_divide_middle = true,
+        },
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {'branch', 'diff', 'diagnostics'},
+          lualine_c = {'filename'},
+          lualine_x = {'encoding', 'fileformat', 'filetype'},
+          lualine_y = {'progress'},
+          lualine_z = {'location'}
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {'filename'},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {}
+        },
+        tabline = {},
+        extensions = {}
+      }
+      require('toggleterm').setup {
+		size = 20,
+		open_mapping = [[<c-t>]],
+		shade_filetypes = {},
+		shade_terminals = true,
+		shading_factor = '1',
+		start_in_insert = true,
+		persist_size = true,
+		direction = 'horizontal'
+     }
+    end
 function init()
-    vim.cmd [[
-	packadd packer.nvim
-	PackerSync
-	]]	
+    vim.cmd [[packadd packer.nvim]]
     installPlugins()
-    require('modes.ide.plugin-conf')	
+    vim.cmd[[PackerSync]]	
+    configure()	
 end
 init()

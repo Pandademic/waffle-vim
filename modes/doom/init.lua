401663f1faa2024c58f9c9e6c0cbc1fa9c7f03e8
Atharv Gupte <74566464+Pandademic@users.noreply.github.com>
2022-01-27 17:30:04 -0500

    Update init.lua

--- modes/doom/init.lua
+++ modes/doom/init.lua
@@ -37,40 +37,46 @@ function installPlugins()
         
 end
 function configure()
-        local alpha = require("alpha")
         local dashboard = require("alpha.themes.dashboard")
-
-        -- Set header
         dashboard.section.header.val = {
-                        
-        "▓█████▄  ▒█████   ▒█████   ███▄ ▄███▓    █     █░ ▄▄▄        █████▒ █████▒██▓    ▓█████", 
-        "▒██▀ ██▌▒██▒  ██▒▒██▒  ██▒▓██▒▀█▀ ██▒   ▓█░ █ ░█░▒████▄    ▓██   ▒▓██   ▒▓██▒    ▓█   ▀", 
-        "░██   █▌▒██░  ██▒▒██░  ██▒▓██    ▓██░   ▒█░ █ ░█ ▒██  ▀█▄  ▒████ ░▒████ ░▒██░    ▒███  ",
-        "░▓█▄   ▌▒██   ██░▒██   ██░▒██    ▒██    ░█░ █ ░█ ░██▄▄▄▄██ ░▓█▒  ░░▓█▒  ░▒██░    ▒▓█  ▄ ",
-        "░▒████▓ ░ ████▓▒░░ ████▓▒░▒██▒   ░██▒   ░░██▒██▓  ▓█   ▓██▒░▒█░   ░▒█░   ░██████▒░▒████▒",
-         "▒▒▓  ▒ ░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒░   ░  ░   ░ ▓░▒ ▒   ▒▒   ▓▒█░ ▒ ░    ▒ ░   ░ ▒░▓  ░░░ ▒░ ░",
-         "░ ▒  ▒   ░ ▒ ▒░   ░ ▒ ▒░ ░  ░      ░     ▒ ░ ░    ▒   ▒▒ ░ ░      ░     ░ ░ ▒  ░ ░ ░  ░",
-         "░ ░  ░ ░ ░ ░ ▒  ░ ░ ░ ▒  ░      ░        ░   ░    ░   ▒    ░ ░    ░ ░     ░ ░      ░   ",
-         "  ░        ░ ░      ░ ░         ░          ░          ░  ░                  ░  ░   ░  ░",
-         "░                                                                                      ",
-         "                                                                                       ",
+          '',
+          '',
+          '888     888  .d8888b.  888b    888                  888     888 d8b               ',
+          '888     888 d88P  Y88b 8888b   888                  888     888 Y8P               ',
+          '888     888 Y88b.      88888b  888                  888     888                   ',
+          'Y88b   d88P  "Y888b.   888Y88b 888  .d88b.   .d88b. Y88b   d88P 888 88888b.d88b.  ',
+          ' Y88b d88P      "Y88b. 888 Y88b888 d8P  Y8b d88""88b Y88b d88P  888 888 "888 "88b ',
+          '  Y88o88P         "888 888  Y88888 88888888 888  888  Y88o88P   888 888  888  888 ',
+          '   Y888P    Y88b  d88P 888   Y8888 Y8b.     Y88..88P   Y888P    888 888  888  888 ',
+          '    Y8P      "Y8888P"  888    Y888  "Y8888   "Y88P"     Y8P     888 888  888  888 ',
         }
-
-        -- Set menu
         dashboard.section.buttons.val = {
-            dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
-            dashboard.button( "f", "  > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
-            dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
-            dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-            dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+                dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+                dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
+                dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+                dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+                dashboard.button("m", "  BookMarks", ":Telescope marks <CR>"),
+          dashboard.button("e", "  Extensions ", ":e ~/.config/nvim/lua/VisualStudioNeovim/Core/plugins.lua<CR>"),
+                dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+                dashboard.button("c", "  Configuration", ":e ~/.config/nvim/lua/VisualStudioNeovim/Core/options.lua<CR>"),
+                dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
         }
 
+
+        dashboard.section.footer.val = {
+          "",
+          "--   DOOM WAFFLE",
+          "",
+        }
+
+        dashboard.section.footer.opts.hl = "Type"
+        dashboard.section.header.opts.hl = "Include"
+        dashboard.section.buttons.opts.hl = "Keyword"
+
+        dashboard.opts.opts.noautocmd = true
+        -- vim.cmd([[autocmd User AlphaReady echo 'ready']])
         alpha.setup(dashboard.opts)
 
-        -- Disable folding on alpha buffer
-        vim.cmd([[
-            autocmd FileType alpha setlocal nofoldenable
-        ]])
 end
 function init()
     installPlugins()

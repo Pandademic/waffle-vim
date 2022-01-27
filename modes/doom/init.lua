c809c0631b1915678f844cf759137447e3d14e09
Atharv Gupte <74566464+Pandademic@users.noreply.github.com>
2022-01-27 17:27:13 -0500

    Update init.lua

--- modes/doom/init.lua
+++ modes/doom/init.lua
@@ -25,7 +25,7 @@ function installPlugins()
                 'goolord/alpha-nvim',
                 requires = { 'kyazdani42/nvim-web-devicons' },
                 config = function ()
-                        require'alpha'.setup(require'alpha.themes.startify'.config)
+                      require'alpha'.setup(require'alpha.themes.dashboard'.config)
                 end
         }
                     
@@ -65,23 +65,6 @@ function configure()
             dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
         }
 
-        -- Set footer
-        --   NOTE: This is currently a feature in my fork of alpha-nvim (opened PR #21, will update snippet if added to main)
-        --   To see test this yourself, add the function as a dependecy in packer and uncomment the footer lines
-        --   ```init.lua
-        --   return require('packer').startup(function()
-        --       use 'wbthomason/packer.nvim'
-        --       use {
-        --           'goolord/alpha-nvim', branch = 'feature/startify-fortune',
-        --           requires = {'BlakeJC94/alpha-nvim-fortune'},
-        --           config = function() require("config.alpha") end
-        --       }
-        --   end)
-        --   ```
-        -- local fortune = require("alpha.fortune") 
-        -- dashboard.section.footer.val = fortune()
-
-        -- Send config to alpha
         alpha.setup(dashboard.opts)
 
         -- Disable folding on alpha buffer

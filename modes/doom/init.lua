9f51bf00d06fc06092351d2fe2d7ab04b543e2e1
Atharv Gupte <74566464+Pandademic@users.noreply.github.com>
2022-01-27 17:17:20 -0500

    Update init.lua

--- modes/doom/init.lua
+++ modes/doom/init.lua
@@ -1,8 +1,36 @@
-vim.cmd [[packadd packer.nvim]]
-vim.cmd [[ PackerSync ]]
-function init()
-		require 'modes.doom.plugins'
+function installPlugins()
+        -- Have packer use a popup window
+    require('packer').init({
+        display = {
+        --open_fn = function()
+        --    return require('packer.util').float({ border = 'single' })
+        --end
+           non_interactive = true
+        }
+     }
+    )
+    return require('packer').startup(function()
+        -- Packer can manage itself
+        use 'wbthomason/packer.nvim'
+      
+        use 'antoinemadec/FixCursorHold.nvim' -- generic dependencie
 
-		require 'modes.doom.configp'
+        use 'Konfekt/FastFold' --  speed up vim
+            
+        use 'tpope/vim-fugitive' -- git wrapping
+        
+         use 'lewis6991/impatient.nvim'
+                    
+          config = {
+        -- Move to lua dir so impatient.nvim can cache it
+        compile_path = vim.fn.stdpath('config')..'/lua/packer_compiled.lua'
+  }
+      end)
+        
+end
+function init()
+    installPlugins()
+    vim.cmd [[packadd packer.nvim]]
+    vim.cmd [[ PackerSync ]]
 end
 init()

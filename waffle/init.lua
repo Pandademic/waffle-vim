54d1180ac249f3105eb8c5f81760247b3f70c73d
Atharv Gupte <74566464+Pandademic@users.noreply.github.com>
2022-01-28 16:03:18 -0500

    Update init.lua

--- waffle/init.lua
+++ waffle/init.lua
@@ -13,21 +13,7 @@ if modes.doom == true then
 		require 'modes.doom'
 end
 -- }
-local vo = require "config.vim"  -- get config
--- load the user vim options {
-vim.opt.cursorline = vo.cursorline
-vim.opt.tabstop =  vo.tabwidth
-vim.bo.autoindent = vo.autoindent
-vim.wo.number = vo.numbers
-vim.bo.smartindent = vo.smartindent
-vim.bo.swapfile = vo.swapfile
-vim.opt.backup = vo.backup
-vim.opt.syntax = vo.syntax
-vim.opt.errorbells = vo.bells
-vim.opt.smartcase = vo.smartcase
-vim.g.mapleader = vo.leaderkey
---- }
-
+require 'waffle.core.loadVimOptions'
 -- basic vim stuff {
 vim.opt.encoding='utf-8'
 vim.opt.mouse='a'

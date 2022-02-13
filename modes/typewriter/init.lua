2b022158edc85efc6bc15320cd3c1ef02da71a66
Atharv Gupte <74566464+Pandademic@users.noreply.github.com>
2022-02-13 16:45:47 -0500

    Update init.lua

--- modes/typewriter/init.lua
+++ modes/typewriter/init.lua
@@ -28,8 +28,7 @@ function installPlugins()
         config = {
                 -- Move to lua dir so impatient.nvim can cache it
                 compile_path = vim.fn.stdpath('config')..'/lua/packer_compiled.lua'
-                }     
-        }
+             }     
 
       end)
         
